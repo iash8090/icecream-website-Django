a77caf5acd1bf0542d4722a5ebe0e6109f3c060c
ASH <25776660+iash8090@users.noreply.github.com>
2023-06-23 19:54:27 +0530

    Update urls.py

--- app1/urls.py
+++ app1/urls.py
@@ -12,10 +12,12 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
 # Dynamic url
     path('icd/<slug:slugID>', views.iceCreamDetails, name='Ice_cream_details'),
-
+    path('cartProduct/', views.cartProduct, name='cartProduct'),
+    
 # API url to GET data using function based View
     path('api/get/', views.get_all_IceCreams, name='get_api'),
 # API url to POST data using Class based View
     path('api/post/', PostView.as_view(), name='post_api'),
+    
 ]
 
